70ee9f00b560c5f58a5b745931265a4a4e557fa5
jdbalantar <jdbalantar@gmail.com>
2022-06-23 22:30:23 -0500

    Utf8 Validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Write a method that determines
+    if a given data set represents a valid UTF-8 encoding.
+"""
+
 def validUTF8(data):
     """
     determines if a given data set
