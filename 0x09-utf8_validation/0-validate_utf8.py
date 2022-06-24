7114c40a0bf90ad794217e9a258ca353379b4cd7
jdbalantar <jdbalantar@gmail.com>
2022-06-23 22:28:20 -0500

    Utf8 Validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+def validUTF8(data):
+    """
+    determines if a given data set
+    represents a valid UTF-8 encoding.
+    """
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode()
+    except:
+        return False
+    return True
